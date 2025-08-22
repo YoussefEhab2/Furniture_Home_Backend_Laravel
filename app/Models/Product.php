8b385b28af3c97828ed6c04ceb6fcd475aeb5185
YoussefEhab2 <youka961@gmail.com>
2025-08-22 23:35:39 +0300

    completed product api's only need the authorization to update it

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'product';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'discount',
+        'category',
+    ];
+    public $timestamps = false;
+}
